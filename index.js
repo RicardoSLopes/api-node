8416d939d0060b61a58634dfd4848f928969edfe
FIAP <FIAP>
2019-10-08 22:31:07 -0300

    testando API

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('meu ambiente',process.env.NODE_ENV);
+//import express from 'express'; tambem funciona
+const express = require('express');
+
+const app = express();
+
+app.get('/usuarios', (request, response, next) =>{
+    console.log("ENTREI NA ROTA USERS");
+    //response.sendStatus(200);
+    response.json({sucess: true})
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
